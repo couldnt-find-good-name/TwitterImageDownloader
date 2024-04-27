ef8bf86fb9890fb2b29c1e301ab41d5b56ae1449
Sean Brar <seanbrar@yahoo.com>
2024-04-27 00:12:55 -0700

    add toggle for new zip downloads

--- src/options.js
+++ src/options.js
@@ -1,16 +1,24 @@
-let dir = document.querySelector('#dir')
-chrome.storage.local.get('path', (result) => {
-	dir.value = result.path || ''
-})
-let id = document.querySelector('#id')
-chrome.storage.local.get('use_id', (result) => {
-	id.checked = result.use_id || false
-})
-function saveOptions () {
-	let path = dir.value.replace(/\/+$/, '') + '/'
-	if (path === '/') {
-		path = ''
-	}
-	chrome.storage.local.set({path, use_id: id.checked})
+let dir = document.querySelector('#dir');
+let id = document.querySelector('#id');
+let useZip = document.querySelector('#use_zip');  // Checkbox for new option
+
+function saveOptions() {
+    let path = dir.value.replace(/\/+$/, '') + '/';
+    path = path === '/' ? '' : path;
+    chrome.storage.local.set({
+        path,
+        use_id: id.checked,
+        use_zip: useZip.checked
+    });
 }
-document.querySelector('form').addEventListener('submit', saveOptions)
+
+function restoreOptions() {
+    chrome.storage.local.get(['path', 'use_id', 'use_zip'], (result) => {
+        dir.value = result.path || '';
+        id.checked = result.use_id || false;
+        useZip.checked = result.use_zip || false;
+    });
+}
+
+document.addEventListener('DOMContentLoaded', restoreOptions);
+document.querySelector('form').addEventListener('submit', saveOptions);
